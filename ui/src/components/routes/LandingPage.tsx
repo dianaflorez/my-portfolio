bdea0a94935dc25e226db774738e51334a182471
Diana Cristy Florez Bravo <dianacristyflorezbravo@Dianas-MacBook-Pro.local>
2025-02-19 14:48:15 -0500

    first part in landing page

--- ui/src/components/routes/LandingPage.tsx
+++ ui/src/components/routes/LandingPage.tsx
@@ -3,9 +3,11 @@ import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import WaveLanding from '../background/WaveLanding';
 import { themes } from '../../styles/ColorStyles';
-import { H1 } from '../../styles/TextStyles';
+import { H1, MediumText } from '../../styles/TextStyles';
 import Lottie from 'lottie-react';
-import animationData from './landing.json';
+import animationData from './landingmia.json';
+import Typewriter from "typewriter-effect";
+
 
 const LandingPage = () => {
   const { t } = useTranslation();
@@ -14,11 +16,43 @@ const LandingPage = () => {
     <Wrapper>
       <WaveLanding />
       <ContentWrapper>
-        <Title>{t('landing.title')}</Title>
+        <TextWrapper>
+          <Title>
+            Hi! I’m Diana,
+            <br />
+            <span>
+              <Typewriter
+                onInit={() => {}}
+                options={{
+                  strings: [
+                    "a Web",
+                    "a Frontend",
+                    "a Mobile",
+                    "a Backend",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+            Developer
+          </Title>
+          <Description>
+            I am dedicated to turning ideas into reality web and mobile applications. 
+          </Description>
+          {/* <SocialWrapper count={socialLinks.length}>
+            {socialLinks.map((item, index) => (
+              <SocialButton icon={item.image} link={item.link} key={index} />
+            ))}
+          </SocialWrapper> */}
+        </TextWrapper>
+
+        <LottieWrapper>
+          <Lottie animationData={animationData} loop={true} />
+        </LottieWrapper>
+
       </ContentWrapper>
-      <LottieWrapper>
-        <Lottie animationData={animationData} loop={true} />
-      </LottieWrapper>
+      
     </Wrapper>
   );
 };
@@ -33,49 +67,84 @@ const Wrapper = styled.div`
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
-  padding: 30px 30px 60px 30px;
+  padding: 170px 30px;
   display: grid;
-
-  @media (max-width: 750px) {
-    grid-template-columns: auto;
-    justify-content: center;
-    gap: 40px;
-  }
+  grid-template-columns: 360px auto;
 
   @media (max-width: 450px) {
-    padding: 10px;
+    grid-template-columns: auto;
+    justify-content: center;
+    padding: 50px 20px 33px;
+    gap: 60px;
+    border: 2px solid pink;
+  }
+`;
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+
+  @media (max-width: 750px) {
+    padding: 0px 20px 10px;
+    
   }
 `;
 
 const Title = styled(H1)`
-  margin-top: 80px;
-  font-size: 70px;
   color: ${themes.dark.text1};
-  text-align: center;
-
-  @media (max-width: 830px) {
-    margin-top: 20px;
-  }
+  background: linear-gradient(180deg,rgb(119, 4, 58) 0%,rgb(173, 93, 193) 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
 
   @media (max-width: 450px) {
-    font-size: 60px;
+    font-size: 48px;
+  }
+
+  span {
+    background: linear-gradient(180deg,rgb(231, 232, 203) 0%, #ffd9b6 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
   }
 `;
+
 const LottieWrapper = styled.div`
-  width: 600px;
-  height: 600px;
-  margin: 0px auto;
+  width: 99%;
+  height: 99%;
+  margin: -190px auto;
   outline: none;
 
   @media (max-width: 750px) {
-    width: 400px;
-    height: 400px;
+    margin: 10px auto;
+    width: 500px;
+    height: 500px;
   }
 
   @media (max-width: 450px) {
-    width: 300px;
-    height: 300px;
+    margin: 10px auto;
+    width: 330px;
+    height: 330px;
   }
 `;
+
+const Description = styled(MediumText)`
+  color: white;
+`;
+
+interface SocialWrapperProps {
+  count: number;
+}
+
+const SocialWrapper = styled.div<SocialWrapperProps>`
+  display: grid;
+  grid-template-columns: repeat(${props => props.count}, auto);
+  gap: 0px;
+  @media (max-width: 450px) {
+    justify-content: space-around;
+  }
+  
+`
 
 export default LandingPage;
